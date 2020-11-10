082243779e89c899593f43bda5f618ac21697948
pascal archambault <pascalarchambault@pop-os.localdomain>
2020-11-10 12:24:40 -0500

    working A*, n x m dimension puzzle

--- a_star.py
+++ a_star.py
@@ -1,8 +1,106 @@
+from copy import deepcopy
+import heapq as hq
+
+class Node:
+
+    def __init__(self, puzzle, parent=None):
+        self.current_state = puzzle
+        self.g = 0
+        self.h = 0
+        self.f = 0
+        self.movement_cost = 0
+        self.parent = parent
+
+    def __lt__(self, other):
+        return self.f < other.f
+
+    def computeChildren(self):
+        puzzle = deepcopy(self.current_state)
+        moves = puzzle.getMoves()
+        children = []
+        
+        for direction, cost in moves:
+            p = deepcopy(puzzle)
+            p.move(direction)
+            node = Node(p, parent=self)
+            node.movement_cost = cost
+            children.append(node)
+            # print(f"Heuristic cost of {direction}: {h_1(p)}")
+        children.sort()
+        self.children = children
+        return children
+
+
+def h_0(puzzle):
+
+    if puzzle.getState()[-1] == 0:
+        return 0
+
+    return 1
 
 def h_1(puzzle):
-    sol = list(range(8))
-    sol.append(sol.pop(0))
-    print(sol)
+    return puzzle.getSumOfPermInv() 
 
-    print(puzzle)
-    return 0
+def contains(node, _list):
+    state = node.current_state.getState()
+
+    for node in _list:
+        if node.current_state.getState() == state:
+            return True
+
+    return False
+
+def add_to_open(open_list, node):
+    for n in open_list:
+        if n.current_state.getState() == node.current_state.getState() and node.f > n.f:
+            return False
+    return True
+
+def is_goal(node):
+    goal = list(range(8))
+    goal.append(goal.pop(0))
+
+    return node.current_state.getState() == goal
+
+def print_moves(node):
+    out = []
+
+    while node.parent != None:
+        out.insert(0,node)
+        node = node.parent
+
+    for n in out:
+        print(n.current_state.printState())
+        print('')
+
+def find_best(puzzle):
+    open_list = []
+    closed_list = []
+
+    node = Node(puzzle)
+    open_list.append(node)
+
+    while open_list != []:
+        open_list.sort(key=lambda x: x.f)
+        current_node = open_list.pop(0)
+        closed_list.append(current_node)
+
+        if is_goal(current_node):
+            print(f"Found solution with cost of {current_node.g}")
+            print_moves(current_node)
+            break
+
+        children = current_node.computeChildren()
+
+        for child_node in children:
+            in_closed = contains(child_node, closed_list)
+            in_open = contains(child_node, open_list)
+
+            if not in_closed:
+                child_node.g = current_node.g + child_node.movement_cost
+                child_node.h = h_1(child_node.current_state)
+                child_node.f = child_node.g + child_node.h
+
+                if add_to_open(open_list, child_node):
+                    open_list.append(child_node)
+                    
--- puzzle.py
+++ puzzle.py
@@ -2,14 +2,17 @@ import a_star as A
 
 class Puzzle:
     #initializes the game board 
-    def __init__(self, puzzle):
+    def __init__(self, puzzle, height, width):
         self.puzzle = puzzle
+        self.height = height
+        self.width = width
         self.cost = 0
 
     #prints the curent state of the board
     def printState(self):
-        print(*self.puzzle[:4], sep = " ")
-        print(*self.puzzle[4:], sep = " ")
+        for i in range(self.height):
+            w = self.width
+            print(*self.puzzle[i*w:(i+1)*w], sep = " ")
 
     def getState(self):
         return self.puzzle
@@ -28,7 +31,9 @@ class Puzzle:
 
     #returns final state
     def getFinal(self):
-        return [1,2,3,4,5,6,7,0]
+        l = list(range(self.width * self.height))
+        l.append(l.pop(0))
+        return l
 
     #private function for swapping positions on the board
     def __swap(self, pos1, pos2):
@@ -37,83 +42,137 @@ class Puzzle:
     #swaps the 0 slot up or down and adds to cost
     def upVerticalMove(self):
         pos = self.puzzle.index(0)
-        if self.puzzle.index(0) < 4:
-            self.__swap(pos, pos+4)
+        w = self.width
+        h = self.height
+
+        if pos + w > len(self.getState()):
+            self.__swap(pos, (pos+w)%w)
         else:
-            self.__swap(pos, pos-4)
+            self.__swap(pos, pos+w)
+
         self.cost += 1
 
     #moves 0 left and adds 1 to cost, if 0 is on the left edge then it wraps around and cost += 2
     def leftHorizontalMove(self):
         pos = self.puzzle.index(0)
-        if pos != 0 and pos != 4:
+        w = self.width
+
+        if pos % w == 0:
+            self.__swap(pos, pos + w - 1)
+            self.cost += 2
+        else:
             self.__swap(pos, pos-1)
-            self.cost += 1
-        elif pos == 0:
-            self.__swap(pos, 3)
-            self.cost += 2
-        elif pos == 4:
-            self.__swap(pos, 7)
-            self.cost += 2
 
     #moves 0 right and adds 1 to cost, if 0 is on the right edge then it wraps around and cost += 2
     def rightHorizontalMove(self):
         pos = self.puzzle.index(0)
-        if pos != 3 and pos != 7:
+        w = self.width
+
+        if pos % w == w - 1:
+            self.__swap(pos, pos - w + 1)
+            self.cost += 2
+        else:
             self.__swap(pos, pos+1)
-            self.cost += 1
-        elif pos == 3:
-            self.__swap(pos, 0)
-            self.cost += 2
-        elif pos == 7:
-            self.__swap(pos, 4)
-            self.cost += 2
 
     #moves the 0 diag left if 0 is in one of the corners and adds 3 to cost
     def leftDiagMove(self):
         pos = self.puzzle.index(0)
-        if pos == 0 or pos == 4:
-            self.__swap(pos, pos-1)
-            self.cost += 3
-        if pos == 3 or pos == 7:
-            self.__swap(pos, (pos-5)%8)
-            self.cost += 3
+        w = self.width
+        h = self.height
+
+        top_l = 0
+        bottom_l = w*(h-1)
+        top_r = w-1
+        bottom_r = w*h-1
+
+        if pos == top_l:
+            self.__swap(pos, bottom_r)
+        elif pos == bottom_l:
+            self.__swap(pos, top_r)
+        elif pos == top_r:
+            self.__swap(pos, pos + w -1)
+        elif pos == bottom_l:
+            self.__swap(pos, pos - w -1)
+        self.cost +=3
+
+
 
     ##moves the 0 diag right if 0 is in one of the corners and adds 3 to cost
     def rightDiagMove(self):
         pos = self.puzzle.index(0)
-        if pos == 0 or pos == 4:
-            self.__swap(pos, (pos+5)%8)
-            self.cost += 3
-        if pos == 3 or pos == 7:
-            self.__swap(pos, (pos+1)%8)
-            self.cost += 3  
+        w = self.width
+        h = self.height
 
-    def getSumOfPermInv(current):
+        top_l = 0
+        bottom_l = w*(h-1)
+        top_r = w-1
+        bottom_r = w*h-1
+
+        if pos == top_l:
+            self.__swap(pos, pos + w + 1)
+        elif pos == bottom_l:
+            self.__swap(pos, pos - w + 1)
+        elif pos == top_r:
+            self.__swap(pos, bottom_l)
+        elif pos == bottom_r:
+            self.__swap(pos, top_l)
+        self.cost += 3
+
+    def getSumOfPermInv(self):
         count = 0 
+        current = self.puzzle
         for x in range(len(current)):
             for y in range(x+1,len(current)):
                 if current[x] > current[y]:
                     count += 1
         return count
 
+    def getManhattanDist(self):
+        total_dist = 0
+        puzzle = self.getState()
+        final = self.getFinal()
+        i=0
+        dist_row = 0
+        for tile in final:
+            j = 0
+            for t in puzzle:
+                if t == tile:
+                    dist_col = j%4 #width
+                    total_dist += dist_row + dist_col
+                    dist_row = 0
+                    break 
+                j += 1
+            i += 1
+            dist_row += 1 if i %2 == 0 else 0
+
+        return total_dist
+
 
     def getMoves(self):
         pos = self.puzzle.index(0)
+        w = self.width
+        h = self.height
+        top_l = 0
+        bottom_l = w*(h-1)
+        top_r = w-1
+        bottom_r = w*h-1
+
         moves = []
         moves.append(('up', 1))
-        if pos == 0 or pos == 4:
+        if pos == top_l or pos == bottom_l:
             moves.append(('left', 2))
             moves.append(('diagLeft', 3))
             moves.append(('diagRight', 3))
         else:
             moves.append(('Left', 1))
-        if pos == 3 or pos == 7:
+        if pos == top_r or pos == bottom_r:
             moves.append(('right', 2))
             moves.append(('diagLeft', 3))
             moves.append(('diagRight', 3))
         else:
             moves.append(('right', 1))
+
+        moves.sort(key=lambda tup: tup[1])
         return moves
 
     def move(self, direction):
@@ -129,8 +188,10 @@ class Puzzle:
             self.leftDiagMove()
 
 
-puzzle = Puzzle([0, 3, 1, 4, 2, 6, 5, 7])
+puzzle = Puzzle([0, 3, 1, 4, 2, 6, 5, 7], 2, 4)
 puzzle.printState()
-A.h_1(puzzle.getState())
+print(puzzle.getManhattanDist())
+# print(puzzle.getFinal())
+# A.find_best(puzzle)
 
 
