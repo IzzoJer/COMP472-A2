ba66d11a8ca0da76003294e8e9317b92ec082daa
IzzoJer <izzo.j@hotmail.com>
2020-11-10 21:11:27 -0500

    workin on analysis part and completed my output files, going to push them when we are ready to submit

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
 from puzzle import Puzzle
 from gbfs import gbfs
 import out
-
-# 3 0 1 4 2 6 5 7
-# 6 3 4 7 1 2 5 0
-# 1 0 3 6 5 2 7 4
-# puzzle = Puzzle([1, 0, 3, 6, 5, 2, 7, 4], 2, 4)
-# puzzle = Puzzle([5, 0 ,8, 4, 2, 1, 7, 3, 6], 3,3)
+import random
 
 # A.find_best(puzzle)
 
-#puzzle = Puzzle([1, 0, 3, 7, 5, 2, 6, 4],2, 4)
+puzzNum = 2
+#puzzle = Puzzle([1, 0, 3, 7, 5, 2, 6, 4],2, 4)   
 #puzzle = Puzzle([0, 3, 1, 4, 2, 6, 5, 7], 2, 4)
 puzzle = Puzzle([1, 0, 3, 7, 5, 2, 6, 4], 2, 4)
 
-# out.solutionFile(gbfs(puzzle, 1)[1:], 'test')
-out.searchFile(gbfs(puzzle, 1)[0], 'test')
+
+# gbfs_h0 = gbfs(puzzle, 0) 
+# gbfs_h1 = gbfs(puzzle, 1)
+# gbfs_h2 = gbfs(puzzle, 2)
+
+#Output
+
+#READ
+#	****look at my return function in gbfs for details on the parameters of the output file functions**********
+#	search takes in a list of lists per itteration so i append a list of [score, score, score, currentState] at each itteration for search
+#	solution file takes in the (finalNode.getSolution(), finalScore, time) is a tuple,
+#	I returned everything i needed when i call my fucntion so everyting is avalable for calling 
+
+# out.solutionFile(gbfs_h1[1:], f'{puzzNum}_gbfs-h1_solution')
+# out.searchFile(gbfs_h1[0], f'{puzzNum}_gbfs-h1_search')
+# out.solutionFile(gbfs_h2[1:], f'{puzzNum}_gbfs-h2_solution')
+# out.searchFile(gbfs_h2[0], f'{puzzNum}_gbfs-h2_search')
+
+
+
+#Analysis
+
+# with open('puzzles.txt', 'w')as f:
+# 	puzz = [1,2,3,4,5,6,7,8]
+# 	for x in range(50):
+# 		random.shuffle(puzz)
+# 		f.write(str(puzz) + '\n')
+
+puzzles = []
+with open('puzzles.txt', 'r')as f:
+	for line in f:
+		line.replace('\'','')
+		line.replace('[','')
+		puzzles.append(line.strip())
+
+for x in puzzles:
+	
