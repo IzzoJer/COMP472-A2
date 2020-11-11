9388d5da40da86ef18aeb56a299be599f562b490
JonathanFrey2704 <jfrey2704@gmail.com>
2020-11-11 12:33:24 -0500

    small fix for analysis part

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ puzzNum = 2
 #puzzle = Puzzle([0, 3, 1, 4, 2, 6, 5, 7], 2, 4)
 puzzle = Puzzle([1, 0, 3, 7, 5, 2, 6, 4], 2, 4)
 
-
 # gbfs_h0 = gbfs(puzzle, 0) 
 # gbfs_h1 = gbfs(puzzle, 1)
 # gbfs_h2 = gbfs(puzzle, 2)
@@ -33,17 +32,14 @@ puzzle = Puzzle([1, 0, 3, 7, 5, 2, 6, 4], 2, 4)
 
 #Analysis
 
-# with open('puzzles.txt', 'w')as f:
-# 	puzz = [1,2,3,4,5,6,7,8]
-# 	for x in range(50):
-# 		random.shuffle(puzz)
-# 		f.write(str(puzz) + '\n')
-
-puzzles = []
-with open('puzzles.txt', 'r')as f:
-	for line in f:
-		line.replace('\'','')
-		line.replace('[','')
-		puzzles.append(line.strip())
+puzzles =[]
+with open('puzzles.txt', 'w')as f:
+	puzz = [1,2,3,4,5,6,7,0]
+	for x in range(50):
+		random.shuffle(puzz)
+		f.write(str(puzz) + '\n')
+		puzzles.append(puzz)
 
 #for x in puzzles:
+#	p = Puzzle(x, 2, 4)
+#	ucs(p)
