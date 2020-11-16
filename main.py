b0f7b92d032ca90bfe6cbabf861eecb7bbdaa340
pascal archambault <pascalarchambault@pop-os.localdomain>
2020-11-16 13:42:03 -0500

    main reads puzzle.txt

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from gbfs import gbfs
 from ucs import ucs
 import out
 import random
+import a_star
 
 # A.find_best(puzzle)
 
 puzzNum = 2
 puzzle = Puzzle([3, 0, 1, 4, 2, 6, 5, 7],2, 4)
+astar_sol = a_star.find_best(puzzle, 2)
 #puzzle = Puzzle([6, 3, 4, 7, 1, 2, 5, 0], 2, 4)
 #puzzle = Puzzle([1, 0, 3, 6, 5, 2, 7, 4], 2, 4)
 
@@ -23,8 +25,8 @@ puzzle = Puzzle([3, 0, 1, 4, 2, 6, 5, 7],2, 4)
 #	solution file takes in the (finalNode.getSolution(), finalScore, time) is a tuple,
 #	I returned everything i needed when i call my fucntion so everyting is avalable for calling 
 
-# out.solutionFile(gbfs_h1[1:], f'{puzzNum}_gbfs-h1_solution')
-# out.searchFile(gbfs_h1[0], f'{puzzNum}_gbfs-h1_search')
+# out.solutionFile(astar_sol[1:], f'{puzzNum}_astar-h2_solution')
+# out.searchFile(astar_sol[0], f'{puzzNum}_astar-h2_search')
 # out.solutionFile(gbfs_h2[1:], f'{puzzNum}_gbfs-h2_solution')
 # out.searchFile(gbfs_h2[0], f'{puzzNum}_gbfs-h2_search')
 #out.solutionFile(ucs(puzzle)[1:], f'{puzzNum}_ucs_solution')
@@ -35,11 +37,7 @@ puzzle = Puzzle([3, 0, 1, 4, 2, 6, 5, 7],2, 4)
 puzzles = []
 nb_puzzles = 50
 with open('puzzles.txt', 'w')as f:
-	puzz = [1,2,3,4,5,6,7,0]
-	for x in range(nb_puzzles):
-		random.shuffle(puzz)
-		f.write(str(puzz) + '\n')
-		puzzles.append(puzz)
+	puzzles = f.readLines()
 
 
 total_length_solution =0
@@ -50,7 +48,7 @@ total_execution_time = 0
 
 for x in puzzles:
 	p = Puzzle(x, 2, 4)
-	output = ucs(p)
+	output = a_star.find_best(p, 1)
 
 	if(output != None):
 		total_length_search += len(output[0])
@@ -66,7 +64,7 @@ average_nb_nosolution = total_nb_nosolution/nb_puzzles
 average_cost = total_cost/nb_puzzles
 average_execution_time = total_execution_time/nb_puzzles
 
-algo_name = 'ucs'
+algo_name = 'astar'
 with open(f'{algo_name}_analysis.txt', 'w')as f:
 	f.write(f'Total length of search: {total_length_search}\n')
 	f.write(f'Total length of solution: {total_length_solution}\n')
