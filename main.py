2f66880fd477adbb01fb6001731ae9fb5157b70a
JonathanFrey2704 <jfrey2704@gmail.com>
2020-11-18 11:59:23 -0500

    analysis

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ puzzNum = 0
 puzzle1 = Puzzle([3, 0, 1, 4, 2, 6, 5, 7],2, 4)
 puzzle2 = Puzzle([6, 3, 4, 7, 1, 2, 5, 0], 2, 4)
 puzzle3 = Puzzle([1, 0, 3, 6, 5, 2, 7, 4], 2, 4)
+#puzzle = Puzzle([3, 2, 4, 1, 0, 7, 6, 5])
 # gbfs_h0 = gbfs(puzzle, 0) 
 # gbfs_h1 = gbfs(puzzle, 1)
 # gbfs_h2 = gbfs(puzzle, 2)
@@ -59,18 +60,18 @@ total_nb_nosolution = 0
 total_cost = 0
 total_execution_time = 0
 
-for x in puzzles:
-	p = Puzzle(x, 2, 4)
-	output = ucs(p)
+#for x in puzzles:
+puzzle = Puzzle([3, 2, 12, 4, 9, 0, 13, 7, 6, 15, 5,14, 8, 1, 10, 11], 4, 4)
+output = a_star.find_best(puzzle, 2)
 
-	if(output != None):
-		total_length_search += len(output[0])
-		total_length_solution += len(output[1])
-		total_cost += output[2]
-		total_execution_time += output[3]
-	else:
-		total_nb_nosolution += 1
-		nb_puzzles -= 1
+if(output != None):
+	total_length_search += len(output[0])
+	total_length_solution += len(output[1])
+	total_cost += output[2]
+	total_execution_time += output[3]
+else:
+	total_nb_nosolution += 1
+	nb_puzzles -= 1
 
 average_length_solution = total_length_solution/nb_puzzles
 average_length_search = total_length_search/nb_puzzles
@@ -78,8 +79,8 @@ average_nb_nosolution = total_nb_nosolution/50
 average_cost = total_cost/nb_puzzles
 average_execution_time = total_execution_time/nb_puzzles
 
-algo_name = 'ucs'
-with open(f'{algo_name}_analysis.txt', 'w')as f:
+algo_name = 'astar-h2'
+with open(f'{algo_name}_scaled_up(4x4).txt', 'w')as f:
 	f.write(f'Total length of search: {total_length_search}\n')
 	f.write(f'Total length of solution: {total_length_solution}\n')
 	f.write(f'Total number of no solution: {total_nb_nosolution}\n')
